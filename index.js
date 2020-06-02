64add21d6286048c58b48ea643a4fe4782bed7d6
develon_cyg <gitee@foxmail.com>
2020-06-03 04:45:49 +0800

    index.js

--- index.js
+++ index.js
@@ -5,78 +5,122 @@ const worker_threads = require('worker_threads');
 const config = require('./config.json');
 
 function main() {
-        let app = new express();
-        app.use('/', express.static(`${__dirname}/webapps`));
+    let app = new express();
+    app.use('/', express.static(`${__dirname}/webapps`));
 
-        app.get(/^\/youtube\/parse$/, (req, res) => {
-                let url = req._parsedUrl.query;
-                console.log({op: '解析', url});
+    app.get(/^\/youtube\/parse$/, (req, res) => {
+        let url = req._parsedUrl.query;
+        console.log({ op: '解析', url });
 
-		let mr = url.match(/^https?:\/\/(?:youtu.be\/|www.youtube.com\/watch\?v=)([\w-]+)$/);
-                if (!!!mr) {
-                        console.log('reject');
-                        res.send({
-                          "error" : "请提供一个Youtube视频URL<br>例如：<br>https://www.youtube.com/watch?v=xxxxxxxxxxx",
-                          "success" : false
-                        });
-                        return;
-                }
+        let mr = url.match(/^https?:\/\/(?:youtu.be\/|www.youtube.com\/watch\?v=)([\w-]{11})$/);
+        if (!!!mr) {
+            console.log('reject');
+            res.send({
+                "error": "请提供一个Youtube视频URL<br>例如：<br>https://www.youtube.com/watch?v=xxxxxxxxxxx",
+                "success": false
+            });
+            return;
+        }
 
-                let thread = new worker_threads.Worker(__filename);
-                thread.once('message', msg => {
-			console.log({msg});
-			res.send(msg);
-                });
-                thread.postMessage({op: 'parse', url, videoID: mr[1]});
+        let thread = new worker_threads.Worker(__filename);
+        thread.once('message', msg => {
+            console.log(JSON.stringify(msg, null, 1));
+            res.send(msg);
         });
+        thread.postMessage({ op: 'parse', url, videoID: mr[1] });
+    });
 
-        app.listen(config.port, config.address, () => {
-		console.log('服务已启动');
-	});
+    app.listen(config.port, config.address, () => {
+        console.log('服务已启动');
+    });
 }
 
 function getAudio(id, format, rate, info, size) {
-	return {id, format, rate, info, size};
+    return { id, format, rate, info, size };
 }
 
 function getVideo(id, format, scale, frame, rate, info, size) {
-	return {id, format, scale, frame, rate, info, size};
+    return { id, format, scale, frame, rate, info, size };
 }
 
 function task() {
-        worker_threads.parentPort.once('message', msg => {
-                switch (msg.op) {
-                        case 'parse': {
-                                let audios = [], videos = [];
-                                let bestAudio = {}, bestVideo = {};
-                                let rs = child_process.execSync(`youtube-dl -F '${ msg.url }' 2> /dev/null`).toString().split('\n');
-                                rs.forEach(it => {
-					let videoRegex1 = /^(\d+)\s+(\w+)\s+(\d+x\d+)\s+(\d+)p\s+(\d+)k , (.*), video only, (.+)MiB$/;
-					let mr = it.match(videoRegex1);
-					if (!!mr) {
-						console.log(getVideo(mr[1], mr[2], mr[3], mr[4], mr[5], mr[6]));
-					}
-                                });
-				worker_threads.parentPort.postMessage({
-					"success" : true,
-					"result" : {
-					    "v" : msg.videoID,
-					    "best" : {
-					      "audio" : bestAudio,
-					      "video" : bestVideo,
-					    },
-					    "available" : {audios, videos}
-					}
-				});
-                        }
-                        case 'download': {
-                        }
-                }
-        });
+    worker_threads.parentPort.once('message', msg => {
+        switch (msg.op) {
+            case 'parse': {
+                let audios = [], videos = [];
+                let bestAudio = {}, bestVideo = {};
+
+                // let rs = child_process.execSync(`youtube-dl -F '${msg.url}' 2> /dev/null`).toString().split('\n');
+                // 测试用数据
+                let rs = `[youtube] sbz3fOe7rog: Downloading webpage
+[youtube] sbz3fOe7rog: Downloading video info webpage
+[info] Available formats for sbz3fOe7rog:
+format code  extension  resolution note
+249          webm       audio only tiny   59k , opus @ 50k (48000Hz), 1.50KB
+251          webm       audio only tiny  150k , opus @160k (48000Hz), 3.85MiB
+250          webm       audio only tiny   78k , opus @ 70k (48000Hz), 2.00MiB
+140          m4a        audio only tiny  129k , m4a_dash container, mp4a.40.2@128k (44100Hz), 3.47MiB
+278          webm       256x144    144p   95k , webm container, vp9, 15fps, video only, 2.36MiB
+160          mp4        256x144    144p  111k , avc1.4d400c, 15fps, video only, 2.95MiB
+133          mp4        426x240    240p  247k , avc1.4d4015, 15fps, video only, 6.58MiB
+242          webm       426x240    240p  162k , vp9, 15fps, video only, 2.62MiB
+18           mp4        512x288    240p  355k , avc1.42001E, mp4a.40.2@ 96k (44100Hz), 9.58MiB (best)`.split('\n');
+
+                rs.forEach(it => {
+                    let videoRegex = /^(\d+)\s+(\w+)\s+(\d+x\d+)\s+(\d+)p\s+(\d+)k , (.*), video only, (.+)MiB$/;
+                    let mr = it.match(videoRegex);
+                    if (!!mr) {
+                        let video = getVideo(mr[1], mr[2], mr[3], mr[4], mr[5], mr[6], mr[7]);
+                        return videos.push(video);
+                    }
+
+                    videoRegex = /^(\d+)\s+(\w+)\s+(\d+x\d+)\s+(\d+)p\s+(\d+)k , (.*), (.+)MiB.+best.+$/;
+                    mr = it.match(videoRegex);
+                    if (!!mr) {
+                        let video = getVideo(mr[1], mr[2], mr[3], mr[4], mr[5], mr[6], mr[7]);
+                        return videos.push(video);
+                    }
+
+                    videoRegex = /^(\d+)\s+(\w+)\s+(\d+x\d+)\s+(?:[^,]+)\s+(\d+)k , (.*), video.*$/;
+                    mr = it.match(videoRegex);
+                    if (!!mr) {
+                        let video = getVideo(mr[1], mr[2], mr[3], 0, mr[4], mr[5], 0);
+                        return videos.push(video);
+                    }
+
+                    let audioRegex = /^(\d+)\s+(\w+)\s+audio only.*\s+(\d+)k , (.*),\s+(?:(.+)MiB|.+)$/;
+                    mr = it.match(audioRegex);
+                    if (!!mr) {
+                        let audio = getAudio(mr[1], mr[2], mr[3], mr[4], mr[5] || '未知');
+                        return audios.push(audio);
+                    }
+                });
+
+                // sort
+                audios.sort((a, b) => a.rate - b.rate);
+                videos.sort((a, b) => a.rate - b.rate);
+                bestAudio = audios[audios.length -1];
+                bestVideo = videos[videos.length -1];
+
+                worker_threads.parentPort.postMessage({
+                    "success": true,
+                    "result": {
+                        "v": msg.videoID,
+                        "best": {
+                            "audio": bestAudio,
+                            "video": bestVideo,
+                        },
+                        "available": { audios, videos }
+                    }
+                });
+            }
+            case 'download': {
+            }
+        }
+    });
 }
 
 if (worker_threads.isMainThread)
-        main();
+    main();
 else
-        task();
-
+    task();
